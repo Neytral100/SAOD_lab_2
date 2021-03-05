6354e74110fe837031fcdf670cba36bb2c5a0024
Neytral100 <rymarev.maksim@mail.ru>
2021-03-05 19:25:09 +0700

    Add function bsrtee_delete

--- src/bstree.c
+++ src/bstree.c
@@ -54,6 +54,62 @@ struct bstree* bstree_lookup(struct bstree* tree, char* key)
     return tree;
 }
 
+void bstree_delete(struct bstree* tree, char* key)
+{
+    struct bstree* node = bstree_lookup(tree, key);
+    struct bstree* parent = tree;
+
+    while ((parent->left != node) && (parent->right != node)) {
+        if ((strcmp(parent->key, node->key)) < 0)
+            parent = parent->left;
+        else
+            parent = parent->right;
+    }
+
+    if ((node->left == NULL) && (node->right == NULL)) {
+        if (parent->left == node) {
+            parent->left = NULL;
+        } else {
+            parent->right = NULL;
+        }
+    }
+
+    if ((node->left != NULL) ^ (node->right != NULL)) {
+        if (parent->left == node) {
+            if (node->left != NULL)
+                parent->left = node->left;
+            else
+                parent->left = node->right;
+        } else {
+            if (node->left != NULL)
+                parent->right = node->left;
+            else
+                parent->right = node->right;
+        }
+    }
+
+    if ((node->left != NULL) && (node->left != NULL)) {
+        struct bstree* min_node = bstree_min(node->right);
+        struct bstree* min_parent = node->right;
+        while ((min_parent->left != min_node)
+               && (min_parent->right != min_node)) {
+            if ((strcmp(min_parent->key, min_node->key)) < 0)
+                min_parent = min_parent->left;
+            else
+                min_parent = min_parent->right;
+        }
+        min_parent->left = min_node->right;
+        if (parent->left == node)
+            parent->left = min_node;
+        else
+            parent->right = min_node;
+        min_node->left = node->left;
+        min_node->right = node->right;
+    }
+
+    free(node);
+}
+
 struct bstree* bstree_min(struct bstree* tree)
 {
     if (tree == NULL) {
--- src/bstree.h
+++ src/bstree.h
@@ -8,5 +8,6 @@ struct bstree {
 struct bstree* bstree_create(char*, int);
 void bstree_add(struct bstree*, char*, int);
 struct bstree* bstree_lookup(struct bstree*, char*);
+void bstree_delete(struct bstree*, char*);
 struct bstree* bstree_min(struct bstree*);
 struct bstree* bstree_max(struct bstree*);
